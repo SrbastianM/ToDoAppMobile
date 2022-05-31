e57a8dcb6e6444cd084ac4ae744e40faa10e6207
unknown <sebas907.l@gmail.com>
2022-05-31 12:33:04 -0500

    Setting another menu where you put on the fav tasks, by the way also you can remove it tasks

--- to_do_app/lib/random_words.dart
+++ to_do_app/lib/random_words.dart
@@ -10,6 +10,7 @@ class RandomWorlds extends StatefulWidget {
 
 class RandomWorldState extends State<RandomWorlds> {
   final _randomWorldPair = <WordPair>[];
+  final _saveRandomWordPair = Set<WordPair>();
 
   Widget _buildList() {
     return ListView.builder(
@@ -28,12 +29,54 @@ class RandomWorldState extends State<RandomWorlds> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySave = _saveRandomWordPair.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: const TextStyle(fontSize: 15.0),
       ),
+      trailing: Icon(
+        alreadySave ? Icons.favorite : Icons.favorite_border,
+        color: alreadySave ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySave) {
+            _saveRandomWordPair.remove(pair);
+          } else {
+            _saveRandomWordPair.add(pair);
+          }
+        });
+      },
     );
+  }
+
+  void _pushSave() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<Widget> tiles = _saveRandomWordPair.map((WordPair pair) {
+        return ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: const TextStyle(fontSize: 18.0),
+          ),
+        );
+      });
+      final List<Widget> divided = ListTile.divideTiles(
+        context: context,
+        tiles: tiles,
+      ).toList();
+
+      return Scaffold(
+        appBar: AppBar(
+            centerTitle: true,
+            title: const Text('SaveValues'),
+            backgroundColor: Colors.purple[800]),
+        body: ListView(
+          children: divided,
+        ),
+      );
+    }));
   }
 
   Widget build(BuildContext context) {
@@ -42,6 +85,12 @@ class RandomWorldState extends State<RandomWorlds> {
         title: const Text('To Do App'),
         centerTitle: true,
         backgroundColor: Colors.purple[800],
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSave,
+          )
+        ],
       ),
       body: _buildList(),
     );
